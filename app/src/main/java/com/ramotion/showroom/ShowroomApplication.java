cd351c36e722b75c584e742032693fccf3d17739
Oleg Vasiliev <oleg.vasiliev@hotmail.com>
2018-01-26 16:26:17 +0300

    Added Leak Canary dependency to detect possible memory leaks. (https://github.com/square/leakcanary)

--- app/src/main/java/com/ramotion/showroom/ShowroomApplication.java
+++ app/src/main/java/com/ramotion/showroom/ShowroomApplication.java
@@ -4,9 +4,23 @@ import android.app.Application;
 
 import com.google.android.gms.analytics.GoogleAnalytics;
 import com.google.android.gms.analytics.Tracker;
+import com.squareup.leakcanary.LeakCanary;
 
 public class ShowroomApplication extends Application {
     private Tracker mTracker;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        if (LeakCanary.isInAnalyzerProcess(this)) {
+            // This process is dedicated to LeakCanary for heap analysis.
+            // You should not init your app in this process.
+            return;
+        }
+        LeakCanary.install(this);
+
+    }
 
     synchronized public Tracker getDefaultTracker() {
         if (mTracker == null) {
